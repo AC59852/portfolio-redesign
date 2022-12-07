7176fc86a23d8dee54d2790f209faddd0de729ba
AC59852 <austin.caron1@gmail.com>
2022-12-07 11:42:15 -0500

    finished static skeleton for about, added static skeleton for work, added contact pop-up, added cta component, reworked aboutherocomponent to be reusable in other pages

---
+++ components/CtaComponent.vue
@@ -0,0 +1,22 @@
+<template>
+  <section class="cta" id="cta">
+    <h2 class="title">Check Out More<span>.</span></h2>
+    <div class="cta__links">
+      <NuxtLink :to="`/${link.route}`" class="cta__link">{{ link.name }}</NuxtLink>
+      <button @click="$nuxt.$emit('openContact')" class="cta__link" v-if="(this.$route.name === 'about' || this.$route.name === 'work')">Contact Me</button>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  props: ['link'],
+
+  methods: {
+  }
+}
+</script>
+
+<style>
+
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,7 @@
 <template>
   <section>
     <NavComponent />
+    <ContactComponent v-if="contact" />
     <Nuxt />
   </section>
 </template>
@@ -8,6 +9,29 @@
 <script>
 export default {
   name: 'Default',
+
+  data() {
+    return {
+      contact: false
+    }
+  },
+
+  created() {
+    this.$nuxt.$on('openContact', () => {
+      this.loadContact()
+    })
+
+    this.$nuxt.$on('closeContact', () => {
+      this.contact = false
+    })
+  },
+
+  methods: {
+    loadContact() {
+      this.contact = true
+    }
+  },
+  
   components: {
     NavComponent: () => import('~/components/NavComponent.vue')
   }
