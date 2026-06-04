e57dadbd96e5031452a1cbd5e1938de747ae0b00
Austin Caron <austin.caron1@gmail.com>
2026-06-04 15:51:27 -0400

    replacing the homepage content, cleaning up styles

--- pages/index.vue
+++ pages/index.vue
@@ -10,64 +10,31 @@
       </h1>
       <img class="home__arrow" src="~/assets/chevron-right-solid.svg" alt="Arrow Pointing downwards indicating to scroll down to view more">
     </section>
-    <!-- <section class="home__growth">
-      <h2 class="title title--growth">Helping You Grow<span class="title--period">.</span></h2>
-      <div class="home__outcomes" v-if="mobile || tablet" @load="cycleOutcome()">
-        <h3 class="home__outcome">{{ currentOutcome.emoji }} {{ currentOutcome.title }}</h3>
-      </div>
-      <div class="home__outcomes" v-else>
-        <h3 class="home__outcome" v-for="outcome in outcomes.slice(0,10)" :key="outcome.id">{{ outcome.emoji }} {{ outcome.title }} <span class="pipe">|</span></h3>
-      </div>
-      <div class="home__growthText">
-        <p>In today's age, having an online presence is crucial for any business looking to grow and reach new customers. A website can help establish credibility, increase visibility, and provide a platform for customers to learn more about your products or services. Having a website can open up new opportunities for your business and help you stay ahead of the competition.</p>
-        <p>I'm here to provide all of that and more. Using my skills and experience as both a designer and developer, I provide beautifully designed, engaging websites that cater to your customers.</p>
-      </div>
-    </section> -->
-    <!-- <section class="home__services">
-      <h2 class="title title--services">My Services<span class="title--period">.</span></h2>
-      <div class="home__cards">
-        <CardComponent v-for="card in cards" :key="card.id" :card="card" />
-      </div>
-    </section>
-    <section class="home__about">
-      <h2 class="title title--bio">About Me<span class="title--period">.</span></h2>
-      <div class="home__aboutWrapper">
-        <div class="image-container"><img src="~/assets/profile_photo.png" alt="Image of Me, Austin Caron, Creative Developer. Brown Eyes, Short Hair, Beanie, Gray Sweater, Small Beard, and a Big Smile" class="home__profileImg image"></div>
-        <div class="home__bio">
-          <h3 class="bio__title about__subtitle">Passionate</h3>
-          <p class="bio__para">I'm a Chatham web designer creating stunning, user-friendly websites. With 5 years of experience, I provide clients with incredible products, and love every minute of it.</p>
-          <div class="bio__links">
-            <nuxt-link to="/about" class="bio__btn bio__btn--dark">Learn More</nuxt-link>
-            <nuxt-link to="/contact" class="bio__btn bio__btn--light">Contact Me</nuxt-link>
-          </div>
-        </div>
-      </div>
-    </section> -->
     <section class="home__projects">
       <h2 class="title title--featured" hidden>Featured Work<span class="title--period">.</span></h2>
       <div class="home__images home__images--top">
-        <a href="https://sparklist.ai" target="_blank" class="image-container home__project home__project--left">
+        <a href="https://blog.austincaron..com" target="_blank" class="image-container home__project home__project--left">
+          <div class="home__info">
+            <h2 class="home__project--heading">Pixels & Pull Requests</h2>
+            <span class="home__plus">+</span>
+          </div>
+          <img class="image" src="~/assets/blog.webp" alt="A tinted laptop on a black couch in a dark gray room. On the laptop screen is a preview image for the Pixels & Pull Requests Blog.">
+        </a>
+        <a href="https://devlift.com/" target="_blank" class="image-container home__project home__project--right">
           <div class="home__info">
             <h2 class="home__project--heading">Sparklist</h2>
             <span class="home__plus">+</span>
           </div>
-          <img class="image" src="~/assets/sparklist.png" style="object-fit: contain !important;" alt="Preview Image of Patrick Bradley's Portfolio website">
-        </a>
-        <a href="https://ixd-weal.web.app/" target="_blank" class="image-container home__project home__project--right">
-          <div class="home__info">
-            <h2 class="home__project--heading">Weal Wellness App</h2>
-            <span class="home__plus">+</span>
-          </div>
-          <img class="image" src="https://images.prismic.io/new-portfolio-ac/2c7addd5-2a39-4624-84a2-94d4a2fb21b8_weal_preview.jpg?auto=compress,format" alt="Preview Image of the Weal - Mental Wellness website">
+          <img class="image" src="~assets/sparklist.png" alt="The foreground contains an iPhone with the Sparklist app open, showing a listing of a lounge char. To the left and right of the phone in the background are two images showing different posts. The left contains a Black Modern Bathroom Vanity for $25, and the right contains Wireless Bluetooth Orange Headphones for $75.">
         </a>
       </div>
       <div class="home__images home__images--mid">
-        <a href="https://toksan-rpg-playground.vercel.app/" target="_blank" class="image-container home__project home__project--left">
+        <a href="https://virtual-visions-red.vercel.app/" target="_blank" class="image-container home__project home__project--left">
           <div class="home__info">
-            <h2 class="home__project--heading">Toksan Game</h2>
+            <h2 class="home__project--heading">Virtual Visions</h2>
             <span class="home__plus">+</span>
           </div>
-          <img class="image" src="https://images.prismic.io/new-portfolio-ac/f1ea1ba9-5b46-4127-81dd-78e6c24cd608_toksan_preview.jpg?auto=compress,format" alt="Two desktop size screens, one with the Toksan.ca splash screen, and the other with the user at home within the game choosing what chapter to play">
+          <img class="image" src="~assets/vv.webp" alt="Two desktop screens beside each other facing diagonally left. The first screen contains a preview of the Virtual Visions profile page. The profile name is Austin Caron, with the profile picture being a photo of Cosmic Spider-Man. It contains their bio, favourite games, and collection of images they have posted. The second screen shows the Virtual Visions login page, with options to login with Google, Apple, Facebook, or email.">
         </a>
       </div>
       <div class="home__images home__images--bot">
@@ -78,12 +45,12 @@
           </div>
           <img class="image" src="https://images.prismic.io/new-portfolio-ac/9664187d-2610-4219-89d4-fa35a31d3433_yt_preview.webp?auto=compress,format" alt="Preview Image for My YouTube Channel: @AustinCaron">
         </a>
-        <a href="https://ac59852.github.io/toksan-anime/" target="_blank" class="image-container home__project home__project--leftSmall">
+        <a href="https://toksan-rpg-playground.vercel.app/" target="_blank" class="image-container home__project home__project--leftSmall">
           <div class="home__info">
-            <h2 class="home__project--heading">Toksan Anime</h2>
+            <h2 class="home__project--heading">Toksan</h2>
             <span class="home__plus">+</span>
           </div>
-          <img class="image" src="https://images.prismic.io/new-portfolio-ac/8b7efcc6-e04e-4508-9d98-17357dec206e_toksan_preview.jpg?auto=compress,format" alt="Preview Image for the Toksan RPG Browser Game">
+          <img class="image" src="https://images.prismic.io/new-portfolio-ac/f1ea1ba9-5b46-4127-81dd-78e6c24cd608_toksan_preview.jpg?auto=compress,format" alt="A glass pane facing diagonally right with two images infront overlapping each other slightly. The first image contains the home page of Toksan, a visual novel RPG. It has a the menu of Toksan and a menu to choose what to do next, with the buttons 'Create Account' and 'Settings visible'. The second image has the first Toksan character in a house setting, with a dialogue box underneath explaining the situation in game. To the right of the character is a story menu allowing the player to choose a chapter.">
         </a>
       </div>
     </section>
@@ -154,60 +121,6 @@ export default {
     return {
       mobile: false,
       tablet: false,
-      // outcomes: [
-      //   {id: 0, emoji: '🥇', title: 'Your Goals First'},
-      //   {id: 1, emoji: '🤝', title: 'User-Centered Design'},
-      //   {id: 2, emoji: '🧘', title: 'Ease of Use'},
-      //   {id: 3, emoji: '🎨', title: 'Branding'},
-      //   {id: 4, emoji: '🔍', title: 'Search Engine Optimization'},
-      //   {id: 5, emoji: '📈', title: 'Conversions'},
-      //   {id: 6, emoji: '📱', title: 'Mobile-Friendly'},
-      //   {id: 7, emoji: '💪', title: 'Success Driven'},
-      //   {id: 8, emoji: '🖥️', title: 'Responsive Design'},
-      //   {id: 9, emoji: '🔥', title: 'Incredible Service'},
-      // ],
-      // currentOutcome: {id: 0, emoji: '🥇', title: 'Your Goals First'},
-      // cards: [
-      //   {
-      //     id: 0, 
-      //     title: 'Starter', 
-      //     price: 500, 
-      //     subtitle: 'Enhance your online presence quickly', 
-      //     points: [
-      //       {id: 0, title: 'High Quality Template'},
-      //       {id: 1, title: 'Blazing Fast Performance'},
-      //       {id: 2, title: 'Responsive Design'},
-      //       {id: 3, title: 'Working Contact Solutions'},
-      //       {id: 4, title: 'Analytics Integration'},
-      //     ]
-      //   },
-      //   {
-      //     id: 1, 
-      //     title: 'Pro', 
-      //     price: 3100, 
-      //     subtitle: 'Phenomenal service free of charge', 
-      //     points: [
-      //       {id: 0, title: 'Personalized Design'},
-      //       {id: 1, title: '10 Pages Plus Revisions'},
-      //       {id: 2, title: 'Dynamic Content Options'},
-      //       {id: 3, title: 'Custom SEO'},
-      //       {id: 4, title: 'Everything From Standard'},
-      //     ]
-      //   },
-      //   {
-      //     id: 2, 
-      //     title: 'Custom', 
-      //     price: 'TBD', 
-      //     subtitle: 'Mix and match for your needs', 
-      //     points: [
-      //       {id: 0, title: 'Content Management Tools'},
-      //       {id: 1, title: 'More Pages/Revisions'},
-      //       {id: 2, title: 'Hosting and Domains'},
-      //       {id: 3, title: 'Monthly Maintenance'},
-      //       {id: 4, title: 'Social Media Integration'},
-      //     ]
-      //   },
-      // ]
     }
   },
 
@@ -226,32 +139,6 @@ export default {
       this.mobile = false
     }
 
-      // set interval for outcomes
-    //   var intervalId = setInterval(() => {
-    //     // fade out
-    //     document.querySelector('.home__outcome').classList.add('home__outcome--fade')
-    //     // fade in
-    //     setTimeout(() => {
-    //       // change outcome
-    //       let random = Math.floor(Math.random() * this.outcomes.length)
-
-    //       if(random === this.currentOutcome.id) {
-    //         random = Math.floor(Math.random() * this.outcomes.length)
-    //       }
-
-    //       this.currentOutcome = this.outcomes[random]
-        
-    //       document.querySelector('.home__outcome').classList.remove('home__outcome--fade')
-    //     }, 1500)
-
-    //   }, 5000)
-    // } else {
-    //   let lastChild = document.querySelector(".home__outcomes").lastChild;
-      
-    //   // remove the pipe from the last child
-    //   lastChild.innerHTML = lastChild.innerHTML.replace('|', '')
-    // }
-
     window.addEventListener('resize', () => {
       if (window.innerWidth < 768) {
         this.mobile = true
@@ -260,18 +147,9 @@ export default {
       } else {
         this.mobile = false
         this.tablet = false
-
-        // let lastChild = document.querySelector(".home__outcomes").lastChild;
-
-        // // remove the pipe from the last child
-        // lastChild.innerHTML = lastChild.innerHTML.replace('|', '')
-
-        // // stop the interval
-        // clearInterval(intervalId)
       }
     })
     
-    //this.smoothScroll()
     this.imageParallax()
   }
 }
